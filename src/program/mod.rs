3e09ec47051f244996ed454a760fbcb43aa6c7bf
Helltraitor <helltraitor@hotmail.com>
2022-01-02 14:01:03 +0300

    Added docs to program

--- src/program/mod.rs
+++ src/program/mod.rs
@@ -1,8 +1,42 @@
+//! Program implementation for Turing Machine RS
+//!
+//! This module provides [`Program`] which is using for initialization
+//! a [`crate::TuringMachine`] and [`ExtendBy`] trait for [`Program`] which
+//! can be used for activating additional functional (pretty usefull).
+//!
+//! # Warning
+//! [`Program`] can panic!
+//!
+//! # Examples
+//! ```rust
+//! use turing_machine_rs::instruction::Direction;
+//! use turing_machine_rs::program::{ExtendBy, Program};
+//!
+//! fn main() {
+//!     let mut program = Program::new(vec!['0', '1'], 3);
+//!     program.extend_by([
+//!         (1, '0', 1, '0', Direction::Right),
+//!         (1, '1', 2, '1', Direction::Right),
+//!         (2, '0', 3, '0', Direction::Left),
+//!         (2, '1', 2, '1', Direction::Right),
+//!         (3, '0', 0, '0', Direction::Center),
+//!         (3, '1', 3, '0', Direction::Left)
+//!     ]);
+//! }
+//! ```
+
 use std::fmt::{Display, Error, Formatter};
 
 use crate::instruction::{Direction, Head, Instruction, Tail};
 use crate::Symbol;
 
+/// Program is a vector-based struct which is implementing minimal api
+/// for changing and extending and no api for removinf and shrink. The Program
+/// for Turing machine have a constant size which equals to
+/// `(STATES.count() - 1) * (ALPHABET.count())`.
+///
+/// If you want to extend program you can use extend method but you must be sure
+/// that this struct can append all these instructions.
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Program<S: Symbol> {
     container: Vec<Instruction<S>>,
@@ -11,6 +45,24 @@ pub struct Program<S: Symbol> {
 }
 
 impl<S: Symbol> Program<S> {
+    /// Constructs a new [`Program`] from vector [`Vec<S>`] and last state
+    /// [`u32`].
+    ///
+    /// # Panics
+    /// Panics when `alphabet.is_empty` or l_state equals to `0`.
+    ///
+    /// # Example
+    /// ```rust
+    /// use turing_machine_rs::program::Program;
+    ///
+    /// fn main() {
+    ///     // This is fine
+    ///     let _ = Program::new(vec!['0'], 1);
+    ///     // These will panic!
+    ///     // let _ = Program::new(vec![], 1);
+    ///     // let _ = Program::new(vec!['0'], 0);
+    /// }
+    /// ```
     pub fn new(alphabet: Vec<S>, l_state: u32) -> Self {
         assert!(!alphabet.is_empty(), "new error: alphabet cannot be empty");
         assert!(
@@ -27,10 +79,25 @@ impl<S: Symbol> Program<S> {
         }
     }
 
+    /// Returns a alphabet reference. Zero cost.
     pub fn alphabet(&self) -> &Vec<S> {
         &self.alphabet
     }
 
+    /// Extends this program by another according to these rules:
+    /// 1. All [`Tail`] parts of instructions of this struct will changes their
+    ///     to `self.l_state` if tail state equals to `0`
+    /// 2. All [`Head`] parts of instructions of another struct will increase
+    ///     by `self.l_state`
+    /// 3. All [`Tail`] parts of instructions of another struct will also
+    ///     increase by `self.l_state` but only if tail state not equals to `0`
+    /// 4. This struct l_state increase by other l_state (really thats happend
+    ///     before setting new instructions)
+    /// # Panics
+    /// Panics when alphabets are different or when this struct can store such
+    /// many instructions. The last is depend to l_state count.
+    ///
+    /// # MUST NOT BE USED
     pub fn extend(&mut self, other: &Program<S>) {
         assert!(
             self.alphabet == other.alphabet,
@@ -62,6 +129,11 @@ impl<S: Symbol> Program<S> {
         }
     }
 
+    /// Returns [`Some(Instruction)`] for [`Head`] if it exitsts in the program
+    /// otherwise [`None`].
+    ///
+    /// # Panics
+    /// Panics when head l_state is large then self l_state.
     pub fn get(&self, head: &Head<S>) -> Option<&Instruction<S>> {
         assert!(
             self.l_state >= head.state,
@@ -74,10 +146,16 @@ impl<S: Symbol> Program<S> {
             .find(|inst: &&Instruction<S>| &inst.head == head)
     }
 
+    /// Returns the program last state.
     pub fn l_state(&self) -> u32 {
         self.l_state
     }
 
+    /// Setting [`Instruction`] to the program.
+    ///
+    /// # Panics
+    /// Panics when head state equals to zero, head or tail symbol
+    /// is not in the alphabet or `self.l_state` is less the head or tail state.
     pub fn set(&mut self, inst: Instruction<S>) {
         assert!(
             inst.head.state != 0,
@@ -107,14 +185,18 @@ impl<S: Symbol> Program<S> {
     }
 }
 
+/// Helper trait which allows to implement extend_by method.
 pub trait ExtendBy<I: ?Sized> {
-    fn extend_by(&mut self, _: I);
+    /// Extends the program with some object depends to realization.
+    fn extend_by(&mut self, iterable: I);
 }
 
 impl<S: Symbol, I> ExtendBy<I> for Program<S>
 where
     I: IntoIterator<Item = (u32, S, u32, S, Direction)>,
 {
+    /// Extends the program by tuple `(u32, S, u32, S, Direction)` first two
+    /// elements are going to [`Head`] and the last three are going to [`Tail`]
     fn extend_by(&mut self, iterable: I) {
         for (h_state, h_symbol, t_state, t_symbol, t_direction) in iterable {
             self.set(Instruction::new(
